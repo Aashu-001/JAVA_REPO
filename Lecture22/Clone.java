fa9bc98567a8a181ccf4d86a53094c90d9e75e98
Aashu-001 <aashusharmaa639827@gmail.com>
2026-08-18 10:31:28 +0530

    Adding Clone.java

---
+++ Lecture22/Clone.java
@@ -0,0 +1,49 @@
+import java.util.Objects;
+ // implements Cloneable -> marker interface
+ // handel cloneNotSyupportException
+ // call super.clone()
+ // always have shallow copy 
+public class Clone {
+    public static void main(String[] args) throws CloneNotSupportedException {
+        Student s1 = new Student(12,"Aashu");
+        Student s2 = (Student)s1.clone();
+
+        System.out.print(s2.age);
+    }
+}
+class Student implements Cloneable{
+    int age;
+    String name;
+    Student(int age,String name){
+        this.age = age;
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object object){
+        //apply constaint 
+        // 1) same class type 
+          if(object == null){
+            return false;
+          }
+          if(this.getClass() != object.getClass()){
+             return false;
+          }
+         
+        Student obj = (Student)object;
+        return this.age == obj.age && this.name == obj.name;
+    }
+    @Override
+    public int hashCode(){
+        // int result = 17; // prime number 
+        // result = result*23+age;
+        // result = result*23+name.hashCode();
+        // return result;
+        return Objects.hash(name,age);
+    }
+    @Override
+    protected Object clone()  throws CloneNotSupportedException{
+        return super.clone();
+    }
+}
+
